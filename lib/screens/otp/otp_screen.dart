78d71470d52b98e51d6a7b7ff517376ecf6c9df4
Pradun Kumar <pradunkumar63@gmail.com>
2024-11-05 13:50:00 +0530

    Step-4

--- lib/screens/otp/otp_screen.dart
+++ lib/screens/otp/otp_screen.dart
@@ -1,12 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/widgets/uihelper.dart';
 
 // ignore: must_be_immutable
 class OtpScreen extends StatelessWidget {
   String phonenumber;
-  OtpScreen({super.key, required this.phonenumber});
 
+  OtpScreen({super.key, required this.phonenumber});
+  TextEditingController otp1Controller = TextEditingController();
+  TextEditingController otp2Controller = TextEditingController();
+  TextEditingController otp3Controller = TextEditingController();
+  TextEditingController otp4Controller = TextEditingController();
+  TextEditingController otp5Controller = TextEditingController();
+  TextEditingController otp6Controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 80,
+            ),
+            Uihelper.CustomText(
+              text: 'Verifying your phone number.',
+              height: 20,
+              fontweight: FontWeight.bold,
+              color: const Color(
+                0xFF00A884,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Uihelper.CustomText(
+              text: "You've tried to register +91${phonenumber}",
+              height: 15,
+            ),
+            Uihelper.CustomText(
+                text: "recently. Wait before request an sms or a call",
+                height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Uihelper.CustomText(
+                  text: 'with your code.',
+                  height: 15,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Uihelper.CustomText(
+                      text: 'Wrong number',
+                      height: 15,
+                      color: const Color(
+                        0xFF00A884,
+                      )),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Uihelper.CustomContainer(otp1Controller),
+                const SizedBox(
+                  width: 10,
+                ),
+                Uihelper.CustomContainer(otp1Controller),
+                const SizedBox(
+                  width: 10,
+                ),
+                Uihelper.CustomContainer(otp1Controller),
+                const SizedBox(
+                  width: 10,
+                ),
+                Uihelper.CustomContainer(otp1Controller),
+                const SizedBox(
+                  width: 10,
+                ),
+                Uihelper.CustomContainer(otp1Controller),
+                const SizedBox(
+                  width: 10,
+                ),
+                Uihelper.CustomContainer(otp1Controller),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+                onPressed: () {},
+                child: Uihelper.CustomText(
+                  text: "Didn't receive code?",
+                  height: 15,
+                  color: const Color(0xFF00A884),
+                ))
+          ],
+        ),
+      ),
+      floatingActionButton:
+          Uihelper.CustomButton(callback: () {}, buttonname: 'Next'),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
--- lib/widgets/uihelper.dart
+++ lib/widgets/uihelper.dart
@@ -8,7 +8,7 @@ class Uihelper {
   }) {
     return SizedBox(
       height: 50,
-      width: 370,
+      width: 350,
       child: ElevatedButton(
           onPressed: () {
             callback();
@@ -35,6 +35,25 @@ class Uihelper {
           fontSize: height,
           color: color ?? const Color(0xFF5E5E5E),
           fontWeight: fontweight),
+    );
+  }
+
+  // ignore: non_constant_identifier_names
+  static CustomContainer(TextEditingController controller) {
+    return Container(
+      height: 40,
+      width: 40,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: const Color(0xFFD9D9D9),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: TextField(
+          controller: controller,
+          decoration: const InputDecoration(border: InputBorder.none),
+        ),
+      ),
     );
   }
 }
